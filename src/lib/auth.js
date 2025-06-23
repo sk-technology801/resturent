11c48a5376256788d206bceae8a79c0db21e7d67
sk-technology801 <sardarsaadisaadi@gmail.com>
2025-06-24 02:32:11 +0500

    push
    gg

--- src/lib/auth.js
+++
@@ -1,7 +0,0 @@
-// lib/auth.js
-import { NextResponse } from 'next/server';
-
-export function isAuthenticated(req) {
-  const token = req.cookies.get('admin-token');
-  return token === process.env.ADMIN_SECRET;
-}
--- src/models/Admin.js
+++
@@ -1,9 +0,0 @@
-// /models/Admin.js
-import mongoose from 'mongoose';
-
-const AdminSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: { type: String },
-});
-
-export default mongoose.models.Admin || mongoose.model('Admin', AdminSchema);
